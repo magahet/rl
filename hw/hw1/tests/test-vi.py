cc26dbc3a984a37746137a2d65cf818e626a3274
Magahet Mendiola <magahet@gmail.com>
2017-05-24 01:07:40 +0000

    hw1 done

--- hw/hw1/tests/test-vi.py
+++ hw/hw1/tests/test-vi.py
@@ -3,20 +3,48 @@ import vi
 
 
 def test_vi():
+    
+    def trunc(v):
+        s = str(v)
+        return s[:min(6, len(s))]
+        
     N=4
     B=(0,1,1,1)
-    expectedValue = 0.25
+    expectedValue = '0.25'
     answer = vi.get_value(N, B)
-    nt.eq_(answer, expectedValue)
+    nt.eq_(trunc(answer), expectedValue)
+    print
 
     N=8
     B=(1,0,1,0,1,1,1,0)
-    expectedValue = 1.8125
+    expectedValue = '1.8125'
     answer = vi.get_value(N, B)
-    nt.eq_(answer, expectedValue)
+    nt.eq_(trunc(answer), expectedValue)
+    print
 
     N=6
     B=(1,0,1,1,0,1)
-    expectedValue = 1.1666
+    expectedValue = '1.1666'
     answer = vi.get_value(N, B)
-    nt.eq_(answer, expectedValue)
+    nt.eq_(trunc(answer), expectedValue)
+    print
+    
+    N=10
+    B=(1,0,1,1,1,1,1,0,1,0)
+    expectedValue='2.06'
+    answer = vi.get_value(N, B)
+    nt.eq_(trunc(answer), expectedValue)
+    print
+     
+    N=18
+    B=(1,0,0,1,0,0,0,1,0,1,0,1,1,1,1,1,0,0)
+    expectedValue='4.9308'
+    answer = vi.get_value(N, B)
+    nt.eq_(trunc(answer), expectedValue)
+    print
+    
+    N=26
+    B=(1,0,0,0,0,1,1,0,1,1,0,1,1,0,0,0,1,1,1,1,1,1,0,0,0,1)
+    expectedValue='6.4733'
+    answer = vi.get_value(N, B)
+    nt.eq_(trunc(answer), expectedValue)
--- hw/hw1/vi.py
+++ hw/hw1/vi.py
@@ -1,13 +1,17 @@
-def get_value(N, B, bankroll=0, depth=1):
+def get_value(N, B, bankroll=0, history=()):
+    history += (bankroll,)
     prob_each = 1.0 / N
     prob_bad = prob_each * sum(B)
     reward = prob_each * sum([i + 1 for i in range(N) if not B[i]])
     expected_value = reward - bankroll * prob_bad
 
     if expected_value > 0:
-        expected_value += max([
-            get_value(N, B, bankroll + i + 1, depth + 1) for
+        future_values = [0] + [
+            get_value(N, B, bankroll + i + 1, history) for
             i in xrange(N) if not B[i]
-        ] + [0]) / (N ** depth)
-        print bankroll, expected_value
+        ]
+        future_value = sum([v for v in future_values if v >= 0])
+        future_value /= float(N)
+        print history, expected_value, future_value
+        expected_value +=  future_value
     return expected_value

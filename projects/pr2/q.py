155c48ae844572aff3d37fa2906a8fe336c5df18
Gar Mendiola <magahet@gmail.com>
2017-06-22 18:54:48 -0700

    blah

--- projects/pr2/q.py
+++ projects/pr2/q.py
@@ -8,10 +8,9 @@ from keras.models import Sequential
 from keras.layers import Dense
 
 
-alpha = 0.3
+alpha = 1.0
 gamma = 0.9
-epsilon = 0.8
-duration = 60
+node_count = 128
 
 
 env = gym.make('LunarLander-v2')
@@ -22,10 +21,12 @@ if len(sys.argv) > 1:
         from keras.models import load_model
         nn = load_model(save_path)
 else:
+    save_path = None
     nn = Sequential()
-    nn.add(Dense(16, input_shape=env.observation_space.shape, activation='relu'))
+    nn.add(Dense(node_count, input_shape=env.observation_space.shape, activation='relu'))
+    nn.add(Dense(node_count, activation='relu'))
     nn.add(Dense(env.action_space.n, activation='linear'))
-    nn.compile(loss='mean_squared_error', optimizer='sgd')
+    nn.compile(loss='mean_squared_error', optimizer='adam')
 
 history = deque()
 rewards = deque()
@@ -35,6 +36,8 @@ start = time.time()
 
 while True:
     episode_num += 1
+    epsilon = 0.0
+    # alpha = 1.0 / episode_num
     state = env.reset()
     cum_rewards = 0
 
@@ -55,6 +58,22 @@ while True:
         if done:
             break
 
+    rewards.append(cum_rewards)
+    if len(rewards) > 100:
+        rewards.popleft()
+
+    avg_rewards = np.mean(rewards)
+
+    if avg_rewards >= 200:
+        break
+
+    if time.time() - start > 5:
+        start = time.time()
+        print episode_num, avg_rewards
+
+    if episode_num % 200 != 0:
+        continue
+
     x = []
     y = []
     for _ in xrange(100):
@@ -65,18 +84,16 @@ while True:
         if done:
             Q_s[action] += reward
         else:
-            Q_s[action] += alpha * (reward + gamma * np.max(Q_s_prime) - Q_s[action])
+            Q_s[action] += alpha * (reward + gamma * np.max(Q_s_prime))
 
         x.append(state)
         y.append(Q_s)
 
     nn.train_on_batch(np.array(x), np.array(y))
+
+
     if save_path:
         nn.save(save_path)
 
-    if time.time() - start > 10:
-        start = time.time()
-        print episode_num, cum_rewards
-
-    if cum_rewards >= 200:
-        break
+if not save_path:
+    nn.save_path('/tmp/trained-model')

e3010aeea39c6135171a74bbc712471c03f2fcc2
Gar Mendiola <magahet@gmail.com>
2017-06-27 01:52:17 -0700

    blah

--- projects/pr2/q.py
+++ projects/pr2/q.py
@@ -2,22 +2,77 @@ import os
 import numpy as np
 from collections import deque
 import gym
-import time
 import sys
-from keras.models import Sequential
-from keras.layers import Dense
+import random
+from keras.models import (Model, Sequential)
+from keras.layers import (Input, Dense, merge, BatchNormalization)
+from keras.optimizers import (Adam, SGD)
+from keras import backend as K
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+seed = 42
 
 
-alpha = 1
+duel = True
+double = True
+priority_replay = True
+
+per_alpha = 0.9
+per_epsilon = 0.01
+
+hidden_nodes = [400, 200]
+optimizer = Adam(lr=0.3)
+activation = 'relu'
+loss = lambda x, y: K.mean(K.sqrt(1 + K.square(x - y)), axis=-1)
+
 gamma = 0.99
-epsilon = 0.6
-epsilon_decay = 0.997
-duration = 60
-num_nodes = 32
+epsilon_end = 500
+epsilon_min = 0.0
+training_freq = 1
+target_update_steps = 1000
+batch_size = 64
+history_size = 5e5
+history_min = 1e3
 
-
+epsilon_delta = (1 - epsilon_min) / epsilon_end
+np.random.seed(seed)
+random.seed(seed)
 env = gym.make('LunarLander-v2')
+env.seed(seed)
+
+
+def hubert_loss(y_true, y_pred):
+    err = y_pred - y_true
+    return K.mean( K.sqrt(1+K.square(err))-1, axis=-1 )
+
+
+def build_nn():
+    if duel:
+        input_ = Input(shape=env.observation_space.shape)
+        h_a = Dense(hidden_nodes[0], activation=activation)(input_) 
+        for node_count in hidden_nodes[1:]:
+            h_a = Dense(node_count, activation=activation)(h_a)
+        advantage = Dense(env.action_space.n)(h_a)
+
+        h_v = Dense(hidden_nodes[0], activation=activation)(input_) 
+        for node_count in hidden_nodes[1:]:
+            h_v = Dense(node_count, activation=activation)(h_v)
+        value = Dense(1)(h_v)
+
+        policy = merge([advantage, value], mode = lambda x: x[0]-K.mean(x[0])+x[1], output_shape = (env.action_space.n,))
+
+        return Model(input=input_,  output=policy)
+    else:
+        nn = Sequential()
+        nn.add(Dense(hidden_nodes[0], activation=activation, input_shape=env.observation_space.shape))
+        for node_count in hidden_nodes[1:]:
+            nn.add(Dense(node_count, activation=activation))
+        nn.add(Dense(env.action_space.n, activation='linear'))
+        return nn
+
+def normalize(vector):
+    return (vector - np.mean(vector)) / np.std(vector)
+
 
 if len(sys.argv) > 1:
     save_path = sys.argv[1]
@@ -26,70 +81,116 @@ if len(sys.argv) > 1:
         nn = load_model(save_path)
 else:
     save_path = None
-    nn = Sequential()
-    nn.add(Dense(num_nodes, input_shape=env.observation_space.shape,
-                 activation='relu'))
-    nn.add(Dense(num_nodes, activation='relu'))
-    nn.add(Dense(env.action_space.n, activation='linear'))
-    nn.compile(loss='mse', optimizer='adam')
+    nn = build_nn()
+    nn.compile(loss=loss, optimizer=optimizer)
+    target_nn = build_nn()
 
-history = deque(maxlen=1e3)
+history = deque(maxlen=history_size)
+priority = deque(maxlen=history_size)
 rewards_history = deque(maxlen=100)
+epsilon = 1.0
 episode_num = 0
 avg_rewards = 0
-start = time.time()
-np.random.seed(42)
+steps_since_update = 0
+render = False
+m = 0
+good = False
 
 while avg_rewards < 200:
     episode_num += 1
     # alpha = 1.0 / episode_num
-    state = env.reset()
+    state = normalize(env.reset())
     episode_reward = 0
     done = False
     steps = 0
+    priority_max = np.max(priority) if priority else per_epsilon
+    priority_mean = np.mean(priority) if priority else per_epsilon
 
-    while not done:
+    while not done and steps < 500:
         steps += 1
-        if steps >= 5000:
-            break
         if np.random.rand() < epsilon:
-            action = env.action_space.sample()
+            action = np.random.choice(env.action_space.n, p=(0.4, 0.2, 0.2, 0.2))
         else:
             Q_s = nn.predict(state.reshape(1, 8))[0]
             action = np.argmax(Q_s)
 
-        if episode_num % 10 == 0:
+        if (render and episode_num % 10 == 0) or episode_num % 50 == 0:
             env.render()
+            if good:
+                print 'OK'
+
         state_prime, reward, done, _ = env.step(action)
-        history.append((state, action, state_prime, reward, done))
         episode_reward += reward
+
+        x, y, vx, vy, angle, v_angle, left_leg, right_leg = state_prime
+        if steps == 500 and not done:
+            reward -= 1000
+        elif left_leg + right_leg != 2 and done:
+            reward -= 1000
+        if all([abs(vx) < 0.2, abs(angle) < 0.1, abs(v_angle) < 0.1, vy > -0.3, vy < -0.2]):
+            good = True
+            reward += 1
+        else:
+            good = False
+        # if action in (1, 3):
+        #    reward -= 0.3
+        # reward -= 4 ** abs(state[2])
+        # reward -= 4 ** abs(state[4])
+        # reward -= 4 ** abs(state[5])
+        history.append((state, action, state_prime, reward, done))
+        priority.append(priority_max)
         state = state_prime
+
+    if not render:
+        render = (avg_rewards > -130 and episode_num > 100) or episode_num >= 799
 
     rewards_history.append(episode_reward)
     avg_rewards = np.mean(rewards_history)
 
-    epsilon = max(epsilon * epsilon_decay, 0.01)
+    epsilon = max(epsilon - epsilon_delta, epsilon_min)
+    if avg_rewards < -100:
+        epsilon = max(epsilon, 0.3)
+    elif avg_rewards < 0:
+        epsilon = max(epsilon, 0.2)
+    elif avg_rewards >= 100:
+        epsilon = 0.1
 
-    #if time.time() - start > 5:
+
     if episode_num % 10 == 0:
-        start = time.time()
-        print episode_num, avg_rewards, epsilon
+        print episode_num, int(episode_reward), int(avg_rewards), epsilon, loss
 
-    if episode_num % 10 != 0:
+    if len(history) < history_min or episode_num % training_freq != 0:
         continue
 
-    for _ in xrange(64):
-        state, action, state_prime, reward, done = (
-            history[np.random.choice(len(history))])
+    sample_weights  = np.power(priority, per_alpha)
+    sample_weights /= np.sum(sample_weights)
+
+    x, y = [], []
+    if priority_replay:
+        samples = np.random.choice(len(history), size=batch_size, replace=False, p=sample_weights)
+    else:
+        samples = np.random.choice(len(history), size=batch_size, replace=False)
+
+    for index in samples:
+        state, action, state_prime, reward, done = history[index]
         Q_s = nn.predict(state.reshape(1, 8))[0]
-        Q_s_prime = nn.predict(state_prime.reshape(1, 8))[0]
+        Q_s_prime = target_nn.predict(state_prime.reshape(1, 8))[0]
 
         if done:
             Q_s[action] = reward
         else:
+            td_error = reward + gamma * np.max(Q_s_prime) - Q_s[action]
+            priority[index] = max(abs(td_error), per_epsilon)
             Q_s[action] = reward + gamma * np.max(Q_s_prime)
 
-        nn.fit(state.reshape(1, 8), Q_s.reshape(1, 4), epochs=1, verbose=False)
+        x.append(state.flatten())
+        y.append(Q_s.flatten())
+    output = nn.fit(np.array(x), np.array(y), verbose=False)
+    loss = sum(output.history.get('loss', []))
 
-    if save_path:
-        nn.save(save_path)
+    if steps_since_update > target_update_steps or not double:
+        steps_since_update = 0
+        target_nn.set_weights(nn.get_weights())
+
+
+nn.save('/tmp/solved-model')
